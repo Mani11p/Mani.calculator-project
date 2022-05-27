030d59faefc5df843bf24f36ccfa2cd8f0b4382a
Mani11p <96407388+Mani11p@users.noreply.github.com>
2022-05-27 23:51:25 +0430

    div I said "calculator" is id put "ltr" for dir colspan is "5" again dir is "qtr" id is calculator or "result" the type is "number" and explain about style

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,2 +1,12 @@
 <template>
   <div>
+  <table id="calculator" dir="ltr" style="text-align: center; margin: auto">
+      <tr>
+        <td colspan="5">
+          <input
+            v-model="screen"
+            dir="ltr"
+            id="result"
+            type="number"
+            style="padding: 5px; font-weight: bold; text-align: right"
+          />
