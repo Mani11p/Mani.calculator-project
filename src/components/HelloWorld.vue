58a3686047a2ef77d6e01218cef5ebb8d9d08670
Mani11p <96407388+Mani11p@users.noreply.github.com>
2022-05-27 23:59:37 +0430

    table buttons now we explain the buttons, sum, minus, mul, equal and others. we explain that each button do what, numbers or other buttons for example button 1, put 1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,3 +10,33 @@
             type="number"
             style="padding: 5px; font-weight: bold; text-align: right"
           />
+          </td>
+        <td></td>
+      </tr>
+      <tr>
+        <td><button @click="digit(7)" class="digit">7</button></td>
+        <td><button @click="digit(8)" class="digit">8</button></td>
+        <td><button @click="digit(9)" class="digit">9</button></td>
+        <td><button @click="div" id="div">/</button></td>
+        <td><button @click="clear" id="clear">C</button></td>
+      </tr>
+      <tr>
+        <td><button @click="digit(4)" class="digit">4</button></td>
+        <td><button @click="digit(5)" class="digit">5</button></td>
+        <td><button @click="digit(6)" class="digit">6</button></td>
+        <td><button @click="mul" id="mul">*</button></td>
+        <td><button @click="back" id="back">del</button></td>
+      </tr>
+      <tr>
+        <td><button @click="digit(1)" class="digit">1</button></td>
+        <td><button @click="digit(2)" class="digit">2</button></td>
+        <td><button @click="digit(3)" class="digit">3</button></td>
+        <td><button @click="minus" id="minus">-</button></td>
+        <td><button @click="mod" id="mod">%</button></td>
+      </tr>
+      <tr>
+        <td colspan="2"><button @click="digit(0)" class="digit">0</button></td>
+        <td><button @click="decimal" id="decimal">.</button></td>
+        <td><button @click="sum" id="sum">+</button></td>
+        <td><button @click="equal" id="equal">=</button></td>
+      </tr>
